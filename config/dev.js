c89e2adbebe89f089268457b5c73c28cf2991d0d
Dhana Krishnasamy <itsmedhana2004@yahoo.com>
2018-08-12 11:58:46 +0100

    redis cache, initial

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
-  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+    "449690327937-mna6fk05dmdc4pdhtehtmn8r70pr2bi1.apps.googleusercontent.com",
+  googleClientSecret: "tVbWXmSwjhr7KgUob6Bwcdh_",
   mongoURI: "mongodb://rwuser:rwuser123@ds119572.mlab.com:19572/blog_dev",
-  cookieKey: "123123123"
+  cookieKey: "ve&y$ec&et"
 };
