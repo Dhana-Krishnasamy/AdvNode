e8301b194d1294da4452ebade8e2b5f694554be4
Dhana Krishnasamy <itsmedhana2004@yahoo.com>
2018-08-13 12:30:08 +0100

    page proxy and login

--- test/header.test.js
+++ test/header.test.js
@@ -1,16 +1,14 @@
-const puppeteer = require("puppeteer");
+const Page = require("./helpers/page");
 
-let page, browser;
+let page;
+
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test("Header has the correct text", async () => {
@@ -26,26 +24,7 @@ test("Clicking login takes to oauth screen", async () => {
 });
 
 test("When signed in, shows Logout button", async () => {
-  const id = "5b6fde09f82cc2db3b7c57f3";
-  const Buffer = require("safe-buffer").Buffer;
-  const sessionObject = {
-    passport: { user: id }
-  };
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    "base64"
-  );
-
-  const Keygrip = require("keygrip");
-  const keys = require("../config/keys");
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign("session=" + sessionString);
-
-  await page.setCookie({ name: "session", value: sessionString });
-  await page.setCookie({ name: "session.sig", value: sig });
-  await page.goto("localhost:3000");
-  //await page.reload();
-  await page.waitFor("a[href='/auth/logout']");
-
+  await page.login();
   const text = await page.$eval("a[href='/auth/logout']", el => el.innerHTML);
   expect(text).toEqual("Logout");
 });
