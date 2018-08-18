d0e79af5eac9309778b75e4d3e865afbf69c3b67
Dhana Krishnasamy <itsmedhana2004@yahoo.com>
2018-08-18 18:47:47 +0100

    travis-ci config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "449690327937-mna6fk05dmdc4pdhtehtmn8r70pr2bi1.apps.googleusercontent.com",
   googleClientSecret: "tVbWXmSwjhr7KgUob6Bwcdh_",
-  mongoURI: "mongodb://rwuser:rwuser123@ds119572.mlab.com:19572/blog_dev",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   redisUrl: "redis://127.0.0.1:6379",
   cookieKey: "ve&y$ec&et"
 };
